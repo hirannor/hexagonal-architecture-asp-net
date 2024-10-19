4ce044729d0bc050b0affb78ee8afb15fc6b5f00
Mate Karolyi <mate_karolyi@outlook.com>
2024-10-19 15:34:42 +0200

    Add logging to email adapter

--- HexagonalArchitecture/Adapter/Notification/Email/EmailNotification.cs
+++ HexagonalArchitecture/Adapter/Notification/Email/EmailNotification.cs
@@ -8,23 +8,45 @@ namespace HexagonalArchitecture.Adapter.Notification.Email;
 public class EmailNotification(ILogger<EmailNotification> logger, IOptions<EmailSettings> settings)
     : IEmailNotification, IDisposable
 {
+    private const string SendEmailNotificationCmdIsNull = "SendEmailNotification command should be not null!";
+
     private readonly EmailSettings _emailSettings = settings.Value;
     private readonly SmtpClient _smtpClient = new();
 
     public async Task Send(SendEmailNotification cmd)
     {
+        if (cmd == null)
+        {
+            logger.LogError(SendEmailNotificationCmdIsNull);
+            ArgumentNullException.ThrowIfNull(SendEmailNotificationCmdIsNull);
+        }
+
+        logger.LogDebug("Email notification adapter triggered...");
+        
         var message = CreateMimeMessage(cmd);
 
         try
         {
+            logger.LogDebug("Establishing connection for SMTP server {server}:{port}",
+                _emailSettings.SmtpServer, _emailSettings.Port);
+
             await _smtpClient.ConnectAsync(
                 _emailSettings.SmtpServer,
                 _emailSettings.Port,
                 MailKit.Security.SecureSocketOptions.StartTls
             );
+            
+            logger.LogDebug("Connection established successfully to SMTP server.");
 
+            logger.LogDebug("Attempting to authenticate to server....");
             await _smtpClient.AuthenticateAsync(_emailSettings.Username, _emailSettings.Password);
+            logger.LogDebug("authentication to SMTP server was successful....");
+
+            logger.LogDebug("Attempting to send email to {recipient}....", cmd.EmailAddress);
             await _smtpClient.SendAsync(message);
+            logger.LogDebug("authentication to SMTP server was successful....");
+            
+            logger.LogDebug("Email sent was successful....");
         }
         catch (Exception ex)
         {
